86dedaf5cb921a0da3b7b6cac5594a19378c543c
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-04-01 20:59:38 -0600

    Early Calculation Test

--- QuadraticPrimes.java
+++ QuadraticPrimes.java
@@ -1,6 +1,32 @@
+import java.lang.Math;
+
 public class QuadraticPrimes {
 	
 	public static void main(String[] args) {
-		
+		int test = 0;
+		for(int n = 0; n < 39; n++) {
+		    test = ((n * n) +3 * n + 41);
+		    if(checkPrime(test)) {
+		        System.out.println("This is prime " + test);
+		    }
+		    else {
+		        System.out.println("This is not prime " + test);
+		    }
+		}
+	}
+	
+	private static boolean checkPrime(int checkNum) {
+	    int i = 2;
+	    double maxCheck;
+	    
+	    maxCheck = Math.sqrt((double) checkNum);
+	    
+	    while(i <= maxCheck) {
+	        if(checkNum % i == 0) {
+	            return false;
+	        }
+	        i++;
+	    }
+	    return true;
 	}
 }
