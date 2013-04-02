0488548f3a05b28bd77b7cec7f6494e51fabf428
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-04-01 21:48:42 -0600

    Problem Solved

--- QuadraticPrimes.java
+++ QuadraticPrimes.java
@@ -2,17 +2,36 @@ import java.lang.Math;
 
 public class QuadraticPrimes {
 	
-	public static void main(String[] args) {
-		int test = 0;
-		for(int n = 0; n < 39; n++) {
-		    test = ((n * n) +3 * n + 41);
-		    if(checkPrime(test)) {
-		        System.out.println("This is prime " + test);
-		    }
-		    else {
-		        System.out.println("This is not prime " + test);
-		    }
-		}
+	public static void main(String[] args) {	
+		int testN = 0;
+        int highestA = 0;
+        int highestB = 0;
+        int highestN = 0;
+        
+        for(int a = -1000; a < 1000; a++) {
+            for(int b = -1000; b < 1000; b++) {
+                while(true) {
+                    if(((testN * testN) + (a * testN) + b) < 1) {
+                        testN = 0;
+                        break;
+                    }
+                    if(checkPrime((testN * testN) + (a * testN) + b)) {
+                        testN++;
+                    }
+                    else {
+                        if(testN > highestN) {
+                            highestA = a;
+                            highestB = b;
+                            highestN = testN;
+                        }
+                        testN = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(highestA + " " + highestB + " " + highestN);
+        System.out.println(highestA * highestB);
 	}
 	
 	private static boolean checkPrime(int checkNum) {
